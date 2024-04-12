9a4ab67a0444794be96f150b2c71bf43e9ae9301
AxEsseNse <Angelarkbk@gmail.com>
2024-04-12 19:39:06 +0400

    add start application file

--- start_app.py
+++ start_app.py
@@ -3,5 +3,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    os.chdir("root/projects/my_tutor")
+    os.chdir("/root/projects/my_tutor")
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
